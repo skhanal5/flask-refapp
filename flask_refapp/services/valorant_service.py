ddcbf02e113b22dafc3f8ace4fe3491428ee8796
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-03 16:51:44 -0500

    Update valorant service constructor to inject adapter dependency, add corresponding unit test

--- flask_refapp/services/valorant_service.py
+++ flask_refapp/services/valorant_service.py
@@ -4,8 +4,8 @@ from flask_refapp.adapters.vlr_adapter import VLRAdapter
 
 
 class ValorantService:
-    def __init__(self):
-        self._adapter = VLRAdapter()
+    def __init__(self, adapter: VLRAdapter = None):
+        self._adapter = adapter or VLRAdapter()
 
     def get_health(self) -> Response:
         return self._adapter.get_health_status()
