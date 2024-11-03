ec9b2189d6de492650f000b63a0095b0a86d783d
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-03 16:52:06 -0500

    move assertion in valorant route test over to test method instead of fixture

--- tests/routes/test_valorant.py
+++ tests/routes/test_valorant.py
@@ -11,9 +11,9 @@ def mock_service() -> MagicMock:
     ) as mock_method:
         mock_method.return_value = {"status": "healthy"}
         yield mock_method
-        mock_method.assert_called_once()
 
 
 def test_get_health(client: FlaskClient, mock_service: MagicMock):
     response = client.get("/valorant/health")
     assert response.status_code == 200
+    mock_service.assert_called_once()
