3c92fd783a169611053ec46cf0a663d21b1d9fdf
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-03 16:58:03 -0500

    Use dependency injection in vlr adapter to inject http client, add corresponding unit tests for module

--- flask_refapp/adapters/vlr_adapter.py
+++ flask_refapp/adapters/vlr_adapter.py
@@ -7,8 +7,8 @@ from flask_refapp.clients.request import Request
 class VLRAdapter:
     base_url = "https://vlrggapi.vercel.app"
 
-    def __init__(self):
-        self._client = HTTPClient(base_url=VLRAdapter.base_url)
+    def __init__(self, client: HTTPClient = None):
+        self._client = client or HTTPClient(base_url=VLRAdapter.base_url)
         self._default_headers = {"Content-Type": "application/json"}
 
     def get_health_status(self) -> Response:
