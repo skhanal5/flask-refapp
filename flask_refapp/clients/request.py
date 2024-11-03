168e56749697fabda2038ba9f1946b01cf36898c
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-03 18:47:29 -0500

    Feature/add skeleton (#7)
    * Initial commit: setup poetry project with app and test directories. Add pyproject.toml file
    
    * Add gitignore and ignore idea directory from vcs
    
    * Add all project dependencies, updating poetry.lock and pyproject.toml
    
    Added ruff, pre-commit, pydantic, flask, flask-pydantic, pytest
    
    * Add .pre-commit-config.yaml with ruff setup
    
    setup linting and formatting checks before commits using pre-commit hook
    
    * Update README with instructions on how to use ruff and pre-commit
    
    * Add project directories and corresponding test directories
    
    * Add httpx dependency so we can send http requests using a client library
    
    * Add a new route to the API that interacts with the VLR API
    
    Add a valorant blueprint which invokes a ValorantService. The ValorantService manages all valorant route related logic. It does this by using a downstream VLR.gg API via the VLR Adapter. The VLR Adapter sends requests to the VLR.gg API using the HTTPClient
    
    * Adjust function definition in adapter, blueprint, services to show return values and type hint those return values
    
    Found bug where blueprint handler wasn't returning any values during unit testing
    
    * Update gitignore to ignore all pycache files
    
    * Add conftest.py containing app level pytest fixtures and test_valorant.py which contains unit tests for route handler
    
    conftest.py contains reusable app and a test_client. test_valorant.py tests health endpoint via the test_client and a mock service
    
    * Fix gitignore bug where .pyc files were not being ignored
    
    * Update valorant service constructor to inject adapter dependency, add corresponding unit test
    
    * move assertion in valorant route test over to test method instead of fixture
    
    * Remove all pyc files from git
    
    * Use dependency injection in vlr adapter to inject http client, add corresponding unit tests for module
    
    * Add http client tests

---
+++ flask_refapp/clients/request.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class Request(StrEnum):
+    GET = ("GET",)
+    POST = "POST"
---
+++ flask_refapp/routes/valorant.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+from flask_refapp.services.valorant_service import ValorantService
+
+valorant_bp = Blueprint("valorant", __name__, url_prefix="/valorant")
+
+
+@valorant_bp.route("/health")
+def get_health():
+    # TODO: Make a singleton instance
+    service = ValorantService()
+    return service.get_health()
