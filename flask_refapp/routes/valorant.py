f7d3fbe0c86b9659739970e812e468165554050d
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-03 18:36:14 -0500

    Add run module to run the flask app. Update health endpoint to return json and add return type hint

--- flask_refapp/routes/valorant.py
+++ flask_refapp/routes/valorant.py
@@ -6,7 +6,7 @@ valorant_bp = Blueprint("valorant", __name__, url_prefix="/valorant")
 
 
 @valorant_bp.route("/health")
-def get_health():
+def get_health() -> dict:
     # TODO: Make a singleton instance
     service = ValorantService()
-    return service.get_health()
+    return service.get_health().json()
