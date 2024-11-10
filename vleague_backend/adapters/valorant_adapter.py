da62ef8edd934c8daa4b3881241631fb663da76b
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-11-10 16:48:03 -0500

    Clean up duplicate code

--- vleague_backend/adapters/valorant_adapter.py
+++ vleague_backend/adapters/valorant_adapter.py
@@ -28,17 +28,22 @@ class VLRAdapter(ValorantPort):
         self._client = HTTPClient(base_url="")
 
     def get_health(self):
-        full_url: str = f"{self.vlr_gg_api_base_url}{self.health_endpoint}"
+        full_url = VLRAdapter.get_full_url(
+            base_url=self.vlr_gg_api_base_url, endpoint=self.health_endpoint
+        )
         return self._client.send_request(
             method=Request.GET, headers=self.default_headers, base_url=full_url
         )
 
     @cached_property
     def get_all_player_stats(self) -> DetailedPlayerStats:
+        full_url = VLRAdapter.get_full_url(
+            base_url=self.vlr_gg_api_base_url, endpoint=self.stats_endpoint
+        )
         response = self._client.send_request(
             method=Request.GET,
             headers=self.default_headers,
-            base_url=self.stats_endpoint,
+            base_url=full_url,
             params={"region": self.region, "timestamp": "30"},
         )
         return DetailedPlayerStats(**response.json())
@@ -55,7 +60,9 @@ class VLRAdapter(ValorantPort):
     def get_team(self, team: str) -> DetailedTeamResponse:
         result = self.get_all_teams
         team_id = VLRAdapter.get_team_id_from_teams(result, team)
-        full_url: str = f"{self.vlr_orl_base_url}{self.teams_endpoint}"
+        full_url = VLRAdapter.get_full_url(
+            base_url=self.vlr_orl_base_url, endpoint=self.teams_endpoint
+        )
         params = {"teamid": team_id}
         response = self._client.send_request(
             method=Request.GET,
@@ -63,11 +70,14 @@ class VLRAdapter(ValorantPort):
             base_url=full_url,
             params=params,
         )
+        print(f"Response: {response}")
         return DetailedTeamResponse(**response.json())
 
     @cached_property
     def get_all_teams(self) -> AllTeamsResponse:
-        full_url: str = f"{self.vlr_orl_base_url}{self.teams_endpoint}"
+        full_url = VLRAdapter.get_full_url(
+            base_url=self.vlr_orl_base_url, endpoint=self.teams_endpoint
+        )
         params = {"page": 1, "limit": "all", "region": self.region}
         response = self._client.send_request(
             method=Request.GET,
@@ -85,9 +95,15 @@ class VLRAdapter(ValorantPort):
         team_data = self.get_team(team)
         return team_data.data.results
 
+    # TODO: Hash results {team_name => team}
     @staticmethod
     def get_team_id_from_teams(response: AllTeamsResponse, team: str) -> str:
         for team_data in response.data:
             if team_data.name == team:
                 return team_data.id
+        print(response)
         raise Exception("Failed to get a response")
+
+    @staticmethod
+    def get_full_url(base_url: str, endpoint: str) -> str:
+        return f"{base_url}{endpoint}"
